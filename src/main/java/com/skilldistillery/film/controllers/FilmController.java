75d3482fc8fdcd6fc0acea91c99e8eb42bbc7a16
mjingram <micha.j.ingram@gmail.com>
2021-07-02 17:48:14 -0600

    server should now run, add home jsp file

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FilmController {
+	
 	@RequestMapping(path= {"/", "home.do"})
 	public String index() {
 		return "WEB-INF/home.jsp";
