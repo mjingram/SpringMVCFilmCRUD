ba90ef24a9d3f9a98ab6d367c5bf38e2f32be8f7
David <destrada2193@gmail.com>
2021-07-03 12:10:31 -0700

    completed controller

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,32 +1,50 @@
 package com.skilldistillery.film.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.skilldistillery.film.database.*;
-import com.skilldistillery.film.entities.*;
+import com.skilldistillery.film.database.DatabaseAccessor;
+import com.skilldistillery.film.entities.Film;
 
 @Controller
 public class FilmController {
-	
+
 	@Autowired
 	private DatabaseAccessor dbAccessor;
-	
+
 	public void setDatabaseAccessor(DatabaseAccessor dbAccessor) {
 		this.dbAccessor = dbAccessor;
-		
+
 	}
-	
-	@RequestMapping(path= "home.do", method=RequestMethod.GET)
+
+	@RequestMapping(path = "home.do", method = RequestMethod.GET)
 	public String home() {
 		return "home";
 	}
-	
-	@RequestMapping(path= "/findFilmById.do", params = "id", method=RequestMethod.GET)
+
+	@RequestMapping(path = "mainResult.do", method = RequestMethod.GET)
+	public String mainResultPage() {
+		return "mainResult";
+	}
+
+	@RequestMapping(path = "success.do", method = RequestMethod.GET)
+	public String successPage() {
+		return "success";
+	}
+
+	@RequestMapping(path = "fail.do", method = RequestMethod.GET)
+	public String failPage() {
+		return "fail";
+	}
+
+	@RequestMapping(path = "findFilmById.do", params = "id", method = RequestMethod.GET)
 	public ModelAndView findFilmById(int id) {
 		ModelAndView mv = new ModelAndView();
 		Film f = dbAccessor.findFilmById(id);
@@ -34,59 +52,72 @@ public class FilmController {
 		mv.setViewName("mainResult");
 		return mv;
 	}
-//	@RequestMapping(path= "/home.do", params= "keyword", method = RequestMethod.GET) 
-//	public ModelAndView findFilmByKeyword(String keyword) {
+
+	@RequestMapping(path = "findFilmByKeyword.do", params = "keyword", method = RequestMethod.GET)
+	public ModelAndView findFilmByKeyword(String keyword) {
+		ModelAndView mv = new ModelAndView();
+		List<Film> films = new ArrayList<Film>();
+		mv.addObject("films", films);
+		mv.setViewName("mainResult");
+		return mv;
+	}
+
+	@RequestMapping(path = "newFilm.do", method = RequestMethod.POST)
+	public ModelAndView createFilm(Film film, RedirectAttributes redir) {
+		ModelAndView mv = new ModelAndView();
+		Film newFilm = dbAccessor.createFilm(film);
+		redir.addFlashAttribute("createdFilm", film);
+		if (newFilm != null) {
+			mv.setViewName("success"); // we can always go back and redirect to findFilmById.do
+		} else {
+			mv.setViewName("fail");
+		}
+
+		return mv;
+	}
+
+//	@RequestMapping(path = "filmAdded.do", method = RequestMethod.GET)
+//	public ModelAndView added() {
 //		ModelAndView mv = new ModelAndView();
-//		mv.setViewName("mainResult");
+//		mv.setViewName("result");
 //		return mv;
 //	}
-//	
-//	@RequestMapping(path= "/home.do", method=RequestMethod.POST)
-//	public ModelAndView createFilm(Film film, RedirectAttributes redir) {
+
+	@RequestMapping(path = "deleteFilm.do", method = RequestMethod.POST)
+	public ModelAndView deleteFilm(Film film, RedirectAttributes redir) {
+		ModelAndView mv = new ModelAndView();
+		boolean deletedFilm = dbAccessor.deleteFilm(film);
+		redir.addFlashAttribute("deletedFilm", film);
+
+		if (deletedFilm == true) {
+			mv.setViewName("success");
+
+		} else {
+			mv.setViewName("fail");
+		}
+		return mv;
+	}
+
+//	@RequestMapping(path = "filmDeleted.do", method = RequestMethod.GET)
+//	public ModelAndView deleted() {
 //		ModelAndView mv = new ModelAndView();
-//		redir.addFlashAttribute("createdFilm", film);
-//		Film newFilm = dbAccessor.createFilm(film);
-//		if (newFilm != null) {
-//			mv.setViewName("succss");
-//		}
-//		else {
-//			mv.setViewName("fail");
-//		}
-//		
+//		mv.setViewName("result");
 //		return mv;
 //	}
-//	
-//	
-//	 @RequestMapping(path= "/mainResult.do", params = "name", method=RequestMethod.POST)
-//	 public ModelAndView deleteFilm(Film film, RedirectAttributes redir) {
-//			ModelAndView mv = new ModelAndView();
-//			redir.addFlashAttribute("deletedFilm", film);
-//			boolean deletedFilm = dbAccessor.deleteFilm(film);
-//			if (deletedFilm == true) {
-//				mv.setViewName("succss");
-//			}
-//			else {
-//				mv.setViewName("fail");
-//			}
-//			
-//			return mv;
-//		}
-//	 
-//	 @RequestMapping(path= "/mainResult.do", params = "name", method=RequestMethod.POST)
-//	 public ModelAndView updateFilm(Film film, RedirectAttributes redir) {
-//		 ModelAndView mv = new ModelAndView();
-//		 redir.addFlashAttribute("updatedFilm", film);
-//		 boolean updatedFilm = dbAccessor.updateFilm(film);
-//		 if (updatedFilm == true) {
-//			 mv.setViewName("succss");
-//		 }
-//		 else {
-//			 mv.setViewName("fail");
-//		 }
-//		 
-//		 return mv;
-//	 }
-	
-	
-	
+
+	@RequestMapping(path = "updateFilm.do", method = RequestMethod.POST)
+	public ModelAndView updateFilm(Film film, RedirectAttributes redir) {
+		ModelAndView mv = new ModelAndView();
+		redir.addFlashAttribute("updatedFilm", film);
+		boolean updatedFilm = dbAccessor.updateFilm(film);
+
+		if (updatedFilm == true) {
+			mv.setViewName("succss");
+		} else {
+			mv.setViewName("fail");
+		}
+
+		return mv;
+	}
+
 }
